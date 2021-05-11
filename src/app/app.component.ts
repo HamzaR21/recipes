5e12261aafd1eb1cd5e427aa6dd8dcc403586539
Hamza Radouan <robinmark476@gmail.com>
2021-05-11 17:14:53 +0000

    Testing API calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  readonly ROOT_URL = "https://jsonplaceholder.typicode.com/"
   title = 'recipes';
+
+  userID: number;
+  posts: Observable<any>;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  getData(){
+    let headers = new HttpHeaders().set('AUTH','token');
+    let params = new HttpParams().set('userId', this.userID.toString());
+
+    this.posts = this.http.get(this.ROOT_URL + 'posts',{params,headers})
+  }
 }
